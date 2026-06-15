92a9bec17937241d40c5b9d072ad2495432a6348
nextpks01-gif <nextpks01@gmail.com>
2026-06-15 16:53:10 -0300

    adicionando arquivos

--- UPLOAD_GITHUB_LIMPO/Tela Bradesco Pontos/realtime-config.js
+++
@@ -1,3 +0,0 @@
-// Para Netlify + backend externo, preencha com a URL WebSocket do servidor Node.
-// Exemplo: window.BRADESCO_WS_URL = "wss://seu-backend.onrender.com";
-window.BRADESCO_WS_URL = "";
